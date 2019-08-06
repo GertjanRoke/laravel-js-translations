271c8efa252ef48f2ed6a8c2f8f4528710bb7f56
Gertjan Roke <gertjan@2solar.nl>
2019-08-06 21:05:00 +0200

    Basic setuip
    Signed-off-by: Gertjan Roke <g.a.roke90@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# laravel-js-translations
-Laravel translations for Javascript, Vuejs, React
+# Laravel Javascript Translations
+This package provides all translation functions from Laravel (`__()`, `trans()` and `trans_choice()`) but then as 
+Javascript functions.  
+It also has mixins for Vue and React.
---
+++ src/LaravelJsTranslationsServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Gertjanroke\LaravelJsTranslations;
+
+use Illuminate\Support\ServiceProvider;
+
+class DumpServerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ( $this->app->runningInConsole() ) {
+
+            $this->publishes( [
+                __DIR__ . '/../config/js-translations.php' => $this->path( 'js-translations.php', 'config' ),
+            ], 'config' );
+
+            $this->publishes( [
+                __DIR__ . '/../resources/js/lib/' => $this->path( 'js', 'resources' ),
+            ], 'assets' );
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom( __DIR__ . '/../config/js-translations.php', 'js-translations' );
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function path( $path = '', $basePath = '' )
+    {
+        return app()->basePath( $basePath ) . ( $path ? DIRECTORY_SEPARATOR . $path : $path );
+    }
+}
